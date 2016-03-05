10a4a0e8887f25959f62ef97f9c7dd1bd7a7b82b
Cliff Jonathan <cliffsantoso@gmail.com>
2016-03-06 00:02:19 +0700

    Add example ground+background assets, background static, camera follows player

---
+++ Assets/BackgroundFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.SetParent(Camera.main.gameObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
