99e6d4d001a81c73317b52f706d9e78d99828680
Octavianus Marcel Harjono <octavianusmarcel12@gmail.com>
2016-04-27 03:30:03 +0700

    Add Main Menu, Options still dummy

---
+++ Assets/Menu/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+	public GameObject main;
+	public GameObject credit;
+	public GameObject option;
+
+	// Use this for initialization
+	void Start () {
+		credit.SetActive (false);
+		option.SetActive (false);
+		main.SetActive (true);
+	}
+
+	public void NewGame() {
+		SceneManager.LoadScene (1);
+	}
+
+	public void Option(){
+		credit.SetActive (false);
+		option.SetActive (true);
+		main.SetActive (false);
+	}
+
+	public void Credit(){
+		option.SetActive (false);
+		credit.SetActive (true);
+		main.SetActive (false);
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+	public void ShowMain(){
+		option.SetActive (false);
+		credit.SetActive (false);
+		main.SetActive (true);
+	}
+}
