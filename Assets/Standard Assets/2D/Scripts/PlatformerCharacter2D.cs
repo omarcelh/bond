a4278755b11e1bd2ebe94e1a86b93374e0511f82
Cliff Jonathan <cliffsantoso@gmail.com>
2016-05-18 00:24:16 +0700

    Lompat

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -82,6 +82,7 @@ namespace UnityStandardAssets._2D
                 // Add a vertical force to the player.
                 m_Grounded = false;
                 m_Anim.SetBool("Ground", false);
+                m_Anim.SetTrigger("Jump");
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
             }
         }
