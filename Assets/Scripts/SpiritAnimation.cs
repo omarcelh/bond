34e13b74788c30f169a3db8275ca05bb63688961
Cliff Jonathan <cliffsantoso@gmail.com>
2016-05-17 23:57:26 +0700

    Added Spirit animation

---
+++ Assets/Scripts/SpiritAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiritAnimation : MonoBehaviour {
+
+	public float acceleration = 0.0008f;
+	private float speed = 0;
+	private int direction = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (speed >= 0.012f)
+			direction = -1;
+		if (speed <= -0.012f)
+			direction = 1;
+		speed += direction * acceleration;
+		transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+
+	}
+}
